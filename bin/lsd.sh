c75446996abd7e75e8471f2f634efd8a10ed11eb
Hannah Fouasnon <hannah@zymergen.com>
2016-09-14 18:18:37 -0700

    Add lsd alias.

---
+++ bin/lsd.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+#
+
+lsd='
+
+                 H H  H H
+                  \|  |/
+                 H-C--C     O
+                       \   //
+                        N--C
+                       /    \
+                 H-C--C      C--C      C--C
+                  /|  |\    /   \\    //  \\
+                 H H  H H  C      C--C      C
+                            \    /    \    /
+                             N--C      C==C
+                            /    \    /   |
+                         H-C      C--C    |
+                          /|          \\  |
+                         H H           C--N
+
+
+'
+echo "$lsd"
