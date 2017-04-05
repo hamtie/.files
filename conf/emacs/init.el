cf9a57208bcbbc7265809c6397a6f4f587a86870
Hannah Fouasnon <hanelifou@gmail.com>
2017-04-05 12:51:44 -0700

    Automatically download packages if missing.

--- conf/emacs/init.el
+++ conf/emacs/init.el
@@ -1,3 +1,6 @@
+;; Required packages.
+(setq package-list '(anti-zenburn-theme ac-helm helm-c-yasnippet helm-google better-defaults cedit dired-launch expand-region find-file-in-project helm helm-dired-history helm-dirset helm-package helm-smex helm-themes idle-highlight-mode ido-at-point ido-load-library ido-ubiquitous jdee keychain-environment list-unicode-display log4j-mode magit magit-gh-pulls markdown-mode markdown-mode+ markdown-preview-mode paredit paredit-everywhere paren-completer pug-mode scala-mode smex xterm-color))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -10,14 +13,24 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
+;; Fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; Install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (anti-zenburn-theme ac-helm helm-c-yasnippet helm-google better-defaults cedit dired-launch expand-region find-file-in-project helm helm-dired-history helm-dirset helm-package helm-smex helm-themes idle-highlight-mode ido-at-point ido-load-library ido-ubiquitous jdee keychain-environment list-unicode-display log4j-mode magit magit-gh-pulls markdown-mode markdown-mode+ markdown-preview-mode paredit paredit-everywhere paren-completer pug-mode scala-mode smex xterm-color))))
+
+ ;; '(package-selected-packages (quote()))
+)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
